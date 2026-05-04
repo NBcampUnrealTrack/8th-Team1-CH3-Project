6d8adc44e0cc869be76ca9d81dcc41231bccb47f
timothy1202 <timothy1202@naver.com>
2026-05-04 21:01:24 +0900

    feat: 전투 시스템 기초 구현
    - CombatTypes 공유 타입 정의
    - HitDetector 라인트레이스 및 부위 판정
    - DamageProcessor 대미지 계산 로직

---
+++ Source/Sparta_H/Systems/Private/DamageProcessor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamageProcessor.h"
+
+float UDamageProcessor::CalculateFinalDamage(const FCombatDamageInfo& Info)
+{
+	float DistMult = GetDistanceMultiplier(Info.Distance, Info.WeaponType);
+	float BoneMult = GetBoneMultiplier(Info.HitBone);
+    
+	return Info.BaseDamage * DistMult * BoneMult;
+}
+
+float UDamageProcessor::GetDistanceMultiplier(float Distance, ECombatWeaponType WeaponType)
+{
+	switch (WeaponType)
+	{
+		case ECombatWeaponType::Rifle:
+		case ECombatWeaponType::Pistol:
+			{
+				if (GUN_RANGE_T1>=Distance)
+					return GUN_MULT_T1;
+				else if (GUN_RANGE_T2>=Distance)
+					return GUN_MULT_T2;
+				else if (GUN_RANGE_T3>=Distance)
+					return GUN_MULT_T3;
+				else if (GUN_RANGE_T4>=Distance)
+					return GUN_MULT_T4;
+				else
+					return GUN_MULT_T5;
+			}
+		case ECombatWeaponType::Knife: return 1.0f;
+		case ECombatWeaponType::Rock: return 1.0f;
+		default: return 1.0f;
+	}
+}
+
+float UDamageProcessor::GetBoneMultiplier(EHitBone Bone)
+{
+	switch (Bone)
+	{
+		case EHitBone::Head: return BoneHead;
+		case EHitBone::Torso: return BoneTorso;
+		case EHitBone::Arm: return BoneLimb;
+		case EHitBone::Leg: return BoneLimb;
+		default: return BoneTorso;
+	}
+}
---
+++ Source/Sparta_H/Systems/Public/CombatTypes.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CombatTypes.generated.h"  
+
+UENUM(BlueprintType)
+enum class EHitBone : uint8
+{
+	Head,
+	Torso,
+	Arm,
+	Leg,
+	None
+};
+
+UENUM(BlueprintType)
+enum class ECombatWeaponType : uint8
+{
+	Rifle,
+	Pistol,
+	Knife,
+	Rock,
+	None
+};
+
+USTRUCT(BlueprintType)
+struct FCombatDamageInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	float BaseDamage = 10.0f;
+	
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	float Distance = 0.0f;
+	
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	ECombatWeaponType WeaponType = ECombatWeaponType::None;
+
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	EHitBone HitBone = EHitBone::None;
+};
