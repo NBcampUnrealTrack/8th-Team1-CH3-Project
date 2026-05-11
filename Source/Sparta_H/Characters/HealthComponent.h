a69d72efdec7954f2d5371387ff250c1ddff5119
UNREAL8기\parkh <parkchanheyk@gmail.com>
2026-05-11 12:46:05 +0900

    feat : 캐릭터 컴포넌트 추가

---
+++ Source/Sparta_H/Characters/HealthComponent.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+// 체력 변화를 UI나 캐릭터에게 알리기 위한 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnHealthChanged, float, CurrentHealth, float, MaxHealth, const UDamageType*, DamageType);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeath);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SPARTA_H_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// 데미지 처리 함수 (액터의 OnTakeAnyDamage를 바인딩할 예정)
+	UFUNCTION()
+	void HandleTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHealthChanged OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnDeath OnDeath;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Health")
+	float MaxHealth = 100.f;
+
+	UPROPERTY(VisibleAnywhere, Category = "Health")
+	float CurrentHealth;
+
+		
+};
